258566fb9975ba19e22ee657ffd6ff8cc4406bb4
James Cavaliere <james.cavaliere@gmail.com>
2026-02-12 07:35:37 -0500

    20260212

--- src/Attributes/Route.php
+++ src/Attributes/Route.php
@@ -7,7 +7,36 @@ use Attribute;
 #[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 class Route
 {
+    private string $route;
+    private array  $http_methods;
+    private int    $priority;
+    private string $name;
+
     public function __construct(string $route, array $http_methods, int $priority = 999, string $name = '')
     {
+        $this->route = $route;
+        $this->http_methods = $http_methods;
+        $this->priority = $priority;
+        $this->name = $name;
+    }
+
+    public function getRoute(): string
+    {
+        return $this->route;
+    }
+
+    public function getHttpMethods(): array
+    {
+        return $this->http_methods;
+    }
+
+    public function getPriority(): int
+    {
+        return $this->priority;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
     }
 }
